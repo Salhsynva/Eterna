63dd0230e3c0115e438c1e84e2cefbc96188f873
Taleh Afandi <mtthwmorningstar@gmail.com>
2022-05-31 01:26:09 +0500

    Add project files.

---
+++ Eterna/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using Eterna.Models;
+using Eterna.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eterna.Controllers
+{
+    public class HomeController : Controller
+    {
+        public object HomeViewModel { get; private set; }
+
+        public IActionResult Index()
+        {
+            List<Feature> features = new List<Feature>
+            {
+                new Feature(1,"bi bi-card-checklist","Lorem Ipsum","Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident"),
+                new Feature(2,"bi bi-bar-chart","Dolor Sitema","Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident"),
+                new Feature(3,"bi bi-binoculars","Lorem Ipsum","Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident")
+            };
+
+            List<Service> services = new List<Service>
+            {
+                new Service(1,"bx bxl-dribbble","Lorem Ipsum","Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi"),
+                new Service(2,"bx bx-file","Lorem Ipsum","Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi"),
+                new Service(3,"bx bx-tachometer","Lorem Ipsum","Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi"),
+                new Service(4,"bx bx-world","Lorem Ipsum","Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi"),
+                new Service(5,"bx bx-slideshow","Lorem Ipsum","Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi"),
+                new Service(6,"bx bx-arch","Lorem Ipsum","Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi")
+            };
+
+            HomeVIewModel homeViewModel = new HomeVIewModel()
+            {
+                Features = features,
+                Services = services
+            };
+            return View(homeViewModel);
+        }
+
+        public IActionResult DetailF(int id)
+        {
+            List<Feature> features = new List<Feature>
+            {
+                new Feature(1,"bi bi-card-checklist","Lorem Ipsum","Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident"),
+                new Feature(2,"bi bi-bar-chart","Dolor Sitema","Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident"),
+                new Feature(3,"bi bi-binoculars","Lorem Ipsum","Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident")
+            };
+
+            var element = features.Find(x => x.Id == id);
+
+            return View(element);
+        }
+
+        public IActionResult DetailS(int id)
+        {
+            List<Service> services = new List<Service>
+            {
+                new Service(1,"bx bxl-dribbble","Lorem Ipsum","Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi"),
+                new Service(2,"bx bx-file","Lorem Ipsum","Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi"),
+                new Service(3,"bx bx-tachometer","Lorem Ipsum","Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi"),
+                new Service(4,"bx bx-world","Lorem Ipsum","Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi"),
+                new Service(5,"bx bx-slideshow","Lorem Ipsum","Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi"),
+                new Service(6,"bx bx-arch","Lorem Ipsum","Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi")
+            };
+
+            var element = services.Find(x => x.Id == id);
+
+            return View(element);
+        }
+    }
+}
---
+++ Eterna/Models/Service.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eterna.Models
+{
+    public class Service
+    {
+        public Service(int id,string icon, string title, string body)
+        {
+            this.Id = id;
+            this.Icon = icon;
+            this.Body = body;
+            this.Title = title;
+        }
+        public int Id { get; set; }
+        public string Icon { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
